51e3c2523d501db1ff2a27700a5852176abee1b1
Axel Kittenberger <axkibe@gmail.com>
2012-01-10 16:06:52 +0100

    .

---
+++ cmd-init.js
@@ -0,0 +1,44 @@
+{
+  cmd: 'alter',
+  time: -1,
+  src: {
+    val: {
+      type: 'Space',
+      items: {
+        '0' : {
+          type: 'Note',
+          zone: {
+ 	        pnw : { x: 100, y: 100 },
+	        pse : { x: 300, y: 200 },
+	      },
+	      doc: {
+	         alley : [
+	          {
+                type: 'Para',
+                text: 'If you can dream---and not make dreams your master;',
+              }, {
+                type: 'Para',
+                text: 'If you can think---and not make thoughts your aim,',
+              }, {
+                type: 'Para',
+                text: 'If you can meet with Triumph and Disaster',
+              }, {
+                type: 'Para',
+                text: 'And treat those two impostors just the same',
+              },
+            ],
+          },
+        },
+      },
+      z : {
+        alley : [
+          0,
+	    ],
+      },
+    },
+  },
+  trg: {
+    path: ['welcome'],
+  },
+}
+
