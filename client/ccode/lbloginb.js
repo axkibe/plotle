a2a49a7298743e6c4d022882b63b8250698fb0df
Axel Kittenberger <axkibe@gmail.com>
2012-05-07 15:18:07 +0200

    .

---
+++ client/ccode/lbloginb.js
@@ -0,0 +1,101 @@
+/**                                                      _.._
+                                                      .-'_.._''.
+ __  __   ___       _....._              .          .' .'     '.\
+|  |/  `.'   `.   .'       '.          .'|         / .'                                _.._
+|   .-.  .-.   ' /   .-'"'.  \        (  |        . '            .-,.-~.             .' .._|    .|
+|  |  |  |  |  |/   /______\  |        | |        | |            |  .-. |    __      | '      .' |_
+|  |  |  |  |  ||   __________|    _   | | .'''-. | |            | |  | | .:-`.'.  __| |__  .'     |
+|  |  |  |  |  |\  (          '  .' |  | |/.'''. \. '            | |  | |/ |   \ ||__   __|'-..  .-'
+|  |  |  |  |  | \  '-.___..-~. .   | /|  /    | | \ '.         .| |  '- `" __ | |   | |      |  |
+|__|  |__|  |__|  `         .'.'.'| |//| |     | |  '. `.____.-'/| |      .'.''| |   | |      |  |
+                   `'-.....-.'.'.-'  / | |     | |    `-._____ / | |     / /   | |_  | |      |  '.'
+                                 \_.'  | '.    | '.           `  |_|     \ \._,\ '/  | |      |   /
+                                       '___)   '___)                      `~~'  `"   |_|      `--'
+
+                         ,   ,-,---.  ,                 ,-,---.
+                         )    '|___/  )   ,-. ,-. . ,-.  '|___/
+                        /     ,|   \ /    | | | | | | |  ,|   \
+                        `--' `-^---' `--' `-' `-| ' ' ' `-^---'
+~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~,| ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+                                               `'
+ Login Button on login board.
+ log in
+
+ Authors: Axel Kittenberger
+ License: MIT(Expat), see accompanying 'License'-file
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+/**
+| Imports
+*/
+var CCode;
+var CCustom;
+var Jools;
+var shell;
+
+/**
+| Exports
+*/
+var CMeth = null;
+
+/**
+| Capsule
+*/
+(function(){
+'use strict';
+if (typeof(window) === 'undefined') { throw new Error('this code needs a browser!'); }
+
+var debug    = Jools.debug;
+var immute   = Jools.immute;
+var is       = Jools.is;
+var isnon    = Jools.isnon;
+var log      = Jools.log;
+var subclass = Jools.subclass;
+var Util     = CCode.Util;
+
+/**
+| The container.
+*/
+CMeth = {
+	LoginBoard : {},
+	MainBoard  : {},
+	RegBoard   : {}
+};
+
+/**
+| Constructor
+*/
+var LBLoginB = CCode.LBLoginB = function(twig, board, inherit, name) {
+	CCustom.call(this, twig, board, inherit, name);
+};
+
+subclass(LBLoginB, CCustom);
+
+LBLoginB.prototype.canFocus = function() {
+	return true;
+};
+
+LBLoginB.prototype.specialKey = function(key) {
+	switch (key) {
+	case 'down'  : this.board.cycleFocus(+1);    return;
+	case 'up'    : this.board.cycleFocus(-1);    return;
+	case 'enter' : Util.login(this.board); return;
+	}
+};
+	
+LBLoginB.prototype.input = function(text) {
+	this.board.cockpit.setCurBoard('MainBoard');
+};
+
+LBLoginB.prototype.mousedown = function(p, shift, ctrl) {
+	var r = CCustom.prototype.mousedown.call(this, p, shift, ctrl);
+	if (!r) return r;
+
+	Util.login(this.board);
+	
+	shell.redraw = true;
+	return true;
+};
+
+})();
---
+++ client/ccode/mbrightb.js
@@ -0,0 +1,65 @@
+/**                                                      _.._
+                                                      .-'_.._''.
+ __  __   ___       _....._              .          .' .'     '.\
+|  |/  `.'   `.   .'       '.          .'|         / .'                                _.._
+|   .-.  .-.   ' /   .-'"'.  \        (  |        . '            .-,.-~.             .' .._|    .|
+|  |  |  |  |  |/   /______\  |        | |        | |            |  .-. |    __      | '      .' |_
+|  |  |  |  |  ||   __________|    _   | | .'''-. | |            | |  | | .:-`.'.  __| |__  .'     |
+|  |  |  |  |  |\  (          '  .' |  | |/.'''. \. '            | |  | |/ |   \ ||__   __|'-..  .-'
+|  |  |  |  |  | \  '-.___..-~. .   | /|  /    | | \ '.         .| |  '- `" __ | |   | |      |  |
+|__|  |__|  |__|  `         .'.'.'| |//| |     | |  '. `.____.-'/| |      .'.''| |   | |      |  |
+                   `'-.....-.'.'.-'  / | |     | |    `-._____ / | |     / /   | |_  | |      |  '.'
+                                 \_.'  | '.    | '.           `  |_|     \ \._,\ '/  | |      |   /
+                                       '___)   '___)                      `~~'  `"   |_|      `--'
+
+                    ,-,-,-.   ,-,---. .-,--.       .   .  ,-,---.
+                    `,| | |    '|___/  `|__/ . ,-. |-. |-  '|___/
+                      | ; | .  ,|   \  )| \  | | | | | |   ,|   \
+                      '   `-' `-^---'  `'  ` ' `-| ' ' `' `-^---'
+~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ,|~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+                                                `'
+ Right Button on Mainboard.
+ Help.
+
+ Authors: Axel Kittenberger
+ License: MIT(Expat), see accompanying 'License'-file
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+/**
+| Imports
+*/
+var CCustom;
+var Jools;
+
+/**
+| Exports
+*/
+var CCode;
+
+/**
+| Capsule
+*/
+(function(){
+'use strict';
+if (typeof(window) === 'undefined') { throw new Error('this code needs a browser!'); }
+
+var debug         = Jools.debug;
+var immute        = Jools.immute;
+var is            = Jools.is;
+var isnon         = Jools.isnon;
+var isArray       = Jools.isArray;
+var log           = Jools.log;
+var subclass      = Jools.subclass;
+
+
+/**
+| Constructor
+*/
+var MBRightB = CCode.MBRightB = function(twig, board, inherit, name) {
+	CCustom.call(this, twig, board, inherit, name);
+};
+
+subclass(MBRightB, CCustom);
+
+})();
