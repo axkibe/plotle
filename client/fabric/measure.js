6834ae1dc979e4e193b45228f8d99dee51f561b9
Axel Kittenberger <axkibe@gmail.com>
2012-06-01 13:47:19 +0200

    splitting fabric

---
+++ client/fabric/measure.js
@@ -0,0 +1,68 @@
+/**                                                      _.._
+                                                      .-'_.._''.
+ __  __   ___       _....._              .          .' .'     '.\
+|  |/  `.'   `.   .'       '.          .'|         / .'                                _.._
+|   .-.  .-.   ' /   .-'"'.  \        (  |        . '            .-,.-~.             .' .._|    .|
+|  |  |  |  |  |/   /______\  |        | |        | |            |  .-. |    __      | '      .' |_
+|  |  |  |  |  ||   __________|    _   | | .'''-. | |            | |  | | .:-`.'.  __| |__  .'     |
+|  |  |  |  |  |\  (          '  .' |  | |/.'''. \. '            | |  | |/ |   \ ||__   __|'-..  .-'
+|  |  |  |  |  | \  '-.___..-~. .   | /|  /    | | \ '.         .| |  '- `" __ | |   | |      |  |
+|__|  |__|  |__|  `         .'.'.'| |//| |     | |  '. `.____.-'/| |      .'.''| |   | |      |  |
+                   `'-.....-.'.'.-'  / | |     | |    `-._____ / | |     / /   | |_  | |      |  '.'
+                                 \_.'  | '.    | '.           `  |_|     \ \._,\ '/  | |      |   /
+                                       '___)   '___)                      `~~'  `"   |_|      `--'
+
+                             ,-,-,-.
+                             `,| | |   ,-. ,-. ,-. . . ,-. ,-.
+                               | ; | . |-' ,-| `-. | | |   |-'
+                               '   `-' `-' `-^ `-' `-^ '   `-'
+~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
+ Measures texts.
+
+ Authors: Axel Kittenberger
+ License: MIT(Expat), see accompanying 'License'-file
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+/**
+| Imports
+*/
+var Jools;
+
+/**
+| Exports
+*/
+var Measure = null;
+
+/**
+| Capsule
+*/
+(function(){
+'use strict';
+if (typeof(window) === 'undefined') { throw new Error('this code needs a browser'); }
+
+var debug        = Jools.debug;
+var immute       = Jools.immute;
+var is           = Jools.is;
+var isnon        = Jools.isnon;
+var log          = Jools.log;
+var fixate       = Jools.fixate;
+
+Measure = {
+	init : function() {
+		Measure._canvas = document.createElement('canvas');
+		Measure._cx = this._canvas.getContext('2d');
+	},
+
+	width : function(text) {
+		return Measure._cx.measureText(text).width;
+	}
+};
+
+Object.defineProperty(Measure, 'font', {
+	get: function() { return Measure._cx.font; },
+	set: function(font) { Measure._cx.font = font; }
+});
+
+})();
