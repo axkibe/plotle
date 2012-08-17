5286a71bd254d60fb3235b277ac0686e08f8f784
Axel Kittenberger <axkibe@gmail.com>
2012-08-17 12:01:36 +0200

    working on portals

---
+++ shell/visual/portal.js
@@ -0,0 +1,254 @@
+/**                                               .---.
+.----.     .----..--.                             |   |
+ \    \   /    / |__|                             |   |
+  '   '. /'   /  .--.                             |   |
+  |    |'    /   |  |                       __    |   |
+  |    ||    |   |  |     _     _    _   .:--.'.  |   |
+  '.   `'   .'   |  |   .' |   | '  / | / |   \ | |   |
+   \        /    |  |  .   | /.' | .' | `" __ | | |   |
+    \      /     |__|.'.'| |///  | /  |  .'.''| | |   |
+     '----'        .'.'.-'  /|   `'.  | / /   | |_'---'
+                   .'   \_.' '   .'|  '/\ \._,\ '/
+                              `-'  `--'  `--'  `"
+                .-,--.         .      .
+                 '|__/ ,-. ,-. |- ,-. |
+                 ,|    | | |   |  ,-| |
+                 `'    `-' '   `' `-^ `'
+~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
+ A portal to another space
+
+ Authors: Axel Kittenberger
+ License: MIT(Expat), see accompanying 'License'-file
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+
+/*
+| Export
+*/
+var Visual;
+Visual = Visual || {};
+
+
+/*
+| Imports
+*/
+var Action;
+var config;
+var Euclid;
+var Jools;
+var shell;
+var theme;
+
+
+/*
+| Capsule
+*/
+(function() {
+'use strict';
+
+if (typeof(window) === 'undefined')
+	{ throw new Error('this code needs a browser!'); }
+
+
+/*
+| Constructor.
+*/
+var Portal = Visual.Portal = function(spacename, twig, path)
+{
+	Visual.Item.call(this, spacename, twig, path);
+};
+
+Jools.subclass(Portal, Visual.Item);
+
+
+/*
+| Resize handles to show on notes.
+*/
+Portal.prototype.handles =
+{
+	n  : true,
+	e  : true,
+	s  : true,
+	w  : true
+};
+
+
+/*
+| Returns the portals silhoutte.
+|
+| $zone :  the cache for the items zone
+| zAnchor: if true anchor the silhoute at zero.
+*/
+Portal.prototype.getSilhoutte = function($zone, zAnchor)
+{
+	throw new Error('TODO');
+};
+
+
+/*
+| Sets the items position and size after an action.
+*/
+Portal.prototype.actionstop = function(view, p)
+{
+	throw new Error('TODO');
+	/*
+	var $action = shell.$action;
+	switch ($action.type) {
+		case Action.ITEMDRAG :
+		case Action.ITEMRESIZE :
+			var zone = this.getZone();
+
+			if (this.twig.zone.eq(zone)) return;
+			shell.peer.setZone(this.$path, zone);
+
+			shell.redraw = true;
+			return true;
+		default :
+			return Visual.Item.prototype.actionstop.call(this, view, p);
+	}
+	*/
+};
+
+
+/*
+| Draws the portal.
+|
+| fabric: to draw upon.
+*/
+Portal.prototype.draw = function(fabric, view)
+{
+	var zone  = this.getZone();
+	var vzone = view.rect(zone);
+	var f     = this.$fabric;
+	var sbary = this.scrollbarY;
+
+	// no buffer hit?
+	if (config.debug.noCache || !f ||
+		vzone.width  !== f.width ||
+		vzone.height !== f.height)
+	{
+		f = this.$fabric = new Euclid.Fabric(vzone.width, vzone.height);
+		var doc     = this.$sub.doc;
+		var imargin = this.innerMargin;
+
+		// calculates if a scrollbar is needed
+		var height = doc.getHeight();
+		sbary.visible = height > zone.height - imargin.y;
+
+		var silhoutte = this.getSilhoutte(vzone, true);
+		f.fill(theme.note.style.fill, silhoutte, 'sketch', Euclid.View.proper);
+
+		// draws selection and text
+		sbary.point = Euclid.Point.renew(0, sbary.getPos(), sbary.point);
+		doc.draw(f, view.home(), zone.width, imargin, sbary.point);
+
+		// draws the border
+		f.edge(theme.note.style.edge, silhoutte, 'sketch', Euclid.View.proper);
+	}
+
+	fabric.drawImage(f, vzone.pnw);
+
+	if (sbary.visible) {
+		this.setScrollbar();
+		sbary.draw(fabric, view);
+	}
+};
+
+
+/*
+| Mouse wheel turned.
+*/
+Portal.prototype.mousewheel = function(view, p, dir, shift, ctrl)
+{
+	var dp = view.depoint(p);
+
+	return this.getZone().within(dp);
+};
+
+
+/*
+| Returns the zone of the item.
+| An ongoing action can modify this to be different than meshmashine data.
+*/
+Portal.prototype.getZone = function()
+{
+	var twig    = this.twig;
+	var $action = shell.$action;
+	var max     = Math.max;
+	var min     = Math.min;
+
+	if (!$action || !this.$path.equals($action.itemPath))
+		{ return twig.zone; }
+
+	// FIXME cache the last zone
+
+	switch ($action.type)
+	{
+		case Action.ITEMDRAG:
+			return twig.zone.add(
+				$action.move.x - $action.start.x,
+				$action.move.y - $action.start.y);
+
+		case Action.ITEMRESIZE:
+			var szone = $action.startZone;
+			if (!szone) return twig.zone;
+			var spnw = szone.pnw;
+			var spse = szone.pse;
+			var dx = $action.move.x - $action.start.x;
+			var dy = $action.move.y - $action.start.y;
+			var minw = theme.note.minWidth;
+			var minh = theme.note.minHeight;
+			var pnw, pse;
+
+			switch ($action.align) {
+			case 'n'  :
+				pnw = Euclid.Point.renew(spnw.x, min(spnw.y + dy, spse.y - minh), spnw, spse);
+				pse = spse;
+				break;
+			case 'ne' :
+				pnw = Euclid.Point.renew(
+					spnw.x, min(spnw.y + dy, spse.y - minh), spnw, spse);
+				pse = Euclid.Point.renew(
+					max(spse.x + dx, spnw.x + minw), spse.y, spnw, spse);
+				break;
+			case 'e'  :
+				pnw = spnw;
+				pse = Euclid.Point.renew(max(spse.x + dx, spnw.x + minw), spse.y, spnw, spse);
+				break;
+			case 'se' :
+				pnw = spnw;
+				pse = Euclid.Point.renew(
+					max(spse.x + dx, spnw.x + minw),
+					max(spse.y + dy, spnw.y + minh), spnw, spse);
+				break;
+			case 's' :
+				pnw = spnw;
+				pse = Euclid.Point.renew(spse.x, max(spse.y + dy, spnw.y + minh), spnw, spse);
+				break;
+			case 'sw'  :
+				pnw = Euclid.Point.renew(min(spnw.x + dx, spse.x - minw), spnw.y, spnw, spse);
+				pse = Euclid.Point.renew(spse.x, max(spse.y + dy, spnw.y + minh), spnw, spse);
+				break;
+			case 'w'   :
+				pnw = Euclid.Point.renew(min(spnw.x + dx, spse.x - minw), spnw.y, spnw, spse);
+				pse = spse;
+				break;
+			case 'nw' :
+				pnw = Euclid.Point.renew(
+					min(spnw.x + dx, spse.x - minw),
+					min(spnw.y + dy, spse.y - minh), spnw, spse);
+				pse = spse;
+				break;
+			//case 'c' :
+			default  :
+				throw new Error('unknown align');
+			}
+			return new Euclid.Rect(pnw, pse);
+		default :
+			return twig.zone;
+	}
+};
+
+})();
