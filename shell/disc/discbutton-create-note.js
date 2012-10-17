d8bbfa313a97263dddc1d08ad19ab8a446c764f0
Axel Kittenberger <axkibe@gmail.com>
2012-10-17 22:08:03 +0200

    working on the disc

---
+++ shell/disc/discbutton-create-note.js
@@ -0,0 +1,100 @@
+/*                               _..._
+ _______                      .-'_..._''.
+ \  ___ `'.   .--.          .' .'      '.\
+  ' |--.\  \  |__|         / .'
+  | |    \  ' .--.        . '
+  | |     |  '|  |        | |
+  | |     |  ||  |     _  | |
+  | |     ' .'|  |   .' | . '
+  | |___.' /' |  |  .   | /\ '.          .
+ /_______.'/  |__|.'.'| |// '. `._____.-'/
+ \_______|/     .'.'.-'  /    `-.______ /
+                .'   \_.'              `
+ .-,--.            ,-,---.     .  .
+ ' |   \ . ,-. ,-.  '|___/ . . |- |- ,-. ,-.
+ , |   / | `-. |    ,|   \ | | |  |  | | | |
+ `-^--'  ' `-' `-' `-^---' `-^ `' `' `-' ' '
+ ,--.             .      ,-,-.       .
+ | `-' ,-. ,-. ,-. |- ,-. ` | |   ,-. |- ,-.
+ |   . |   |-' ,-| |  |-'   | |-. | | |  |-'
+ `--'  '   `-' `-^ `' `-'  ,' `-' `-' `' `-'
+
+~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
+ The note button on the creation disc.
+
+ Authors: Axel Kittenberger
+ License: MIT(Expat), see accompanying 'License'-file
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+
+/*
+| Export
+*/
+var Disc;
+Disc = Disc || { };
+
+
+/*
+| Imports
+*/
+var config;
+var Curve;
+var Dash;
+var Design;
+var Euclid;
+var Jools;
+var Proc;
+var shell;
+var theme;
+var Tree;
+
+
+/*
+| Capsule
+*/
+( function( ) {
+'use strict';
+
+
+if( typeof( window ) === 'undefined' )
+	{ throw new Error( 'this code needs a browser!' ); }
+
+/*
+| Constructor
+*/
+var DiscButtonCreateNote = Disc.DiscButtonCreateNote =
+	function( disc )
+{
+	Disc.DiscButton.call(
+		this,
+		disc,
+		'note'
+	);
+};
+
+Jools.subclass( DiscButtonCreateNote, Disc.DiscButton );
+
+
+/*
+| Draws the buttons icon
+*/
+DiscButtonCreateNote.prototype.drawIcon =
+	function(
+		fabric
+	)
+{
+	var wx = 22;
+	var ny = 26;
+
+	fabric.fillText(
+		'note',
+		wx,
+		ny,
+		this.myStyle.font
+	);
+};
+
+
+} )( );
