7e0c14c7c350370012e95a525c355556d3f1f2c8
Axel Kittenberger <axkibe@gmail.com>
2012-10-18 14:07:58 +0200

    working on the disc

---
+++ shell/disc/discbutton-create-label.js
@@ -0,0 +1,100 @@
+/*                               _..._
+ _______                      .-'_..._''.
+ \  ___ `'.   .--.          .' .'      '.\
+  ' |--.\  \  |__|         / .'
+  | |    \  ' .--.        . '
+  | |     |  '|  |        | |
+  | |     |  ||  |     _  | |
+  | |     ' .'|  |   .' | . '
+  | |___.' /' |  |  .   | /\ '.          .
+ /_______.'/  |__|.'.'| |// '. `._____.-'/
+ \_______|/     .'.'.-'  /    `-.______ /
+                .'   \_.'              `
+ .-,--.            ,-,---.     .  .
+ ' |   \ . ,-. ,-.  '|___/ . . |- |- ,-. ,-.
+ , |   / | `-. |    ,|   \ | | |  |  | | | |
+ `-^--'  ' `-' `-' `-^---' `-^ `' `' `-' ' '
+ ,--.             .       ,       .       .
+ | `-' ,-. ,-. ,-. |- ,-.  )   ,-. |-. ,-. |
+ |   . |   |-' ,-| |  |-' /    ,-| | | |-' |
+ `--'  '   `-' `-^ `' `-' `--' `-^ ^-' `-' `'
+
+~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
+ The label button on the creation disc.
+
+ Authors: Axel Kittenberger
+ License: MIT(Expat), see accompanying 'License'-file
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+
+/*
+| Export
+*/
+var Disc;
+Disc = Disc || { };
+
+
+/*
+| Imports
+*/
+var config;
+var Curve;
+var Dash;
+var Design;
+var Euclid;
+var Jools;
+var Proc;
+var shell;
+var theme;
+var Tree;
+
+
+/*
+| Capsule
+*/
+( function( ) {
+'use strict';
+
+
+if( typeof( window ) === 'undefined' )
+	{ throw new Error( 'this code needs a browser!' ); }
+
+/*
+| Constructor
+*/
+var DiscButtonCreateLabel = Disc.DiscButtonCreateLabel =
+	function( disc )
+{
+	Disc.DiscButton.call(
+		this,
+		disc,
+		'label'
+	);
+};
+
+Jools.subclass( DiscButtonCreateLabel, Disc.DiscButton );
+
+
+/*
+| Draws the buttons icon
+*/
+DiscButtonCreateLabel.prototype.drawIcon =
+	function(
+		fabric
+	)
+{
+	var wx = 30;
+	var ny = 35;
+
+	fabric.fillText(
+		'label',
+		wx,
+		ny,
+		this.myStyle.font
+	);
+};
+
+
+} )( );
---
+++ shell/disc/discbutton-create-portal.js
@@ -0,0 +1,99 @@
+/*                               _..._
+ _______                      .-'_..._''.
+ \  ___ `'.   .--.          .' .'      '.\
+  ' |--.\  \  |__|         / .'
+  | |    \  ' .--.        . '
+  | |     |  '|  |        | |
+  | |     |  ||  |     _  | |
+  | |     ' .'|  |   .' | . '
+  | |___.' /' |  |  .   | /\ '.          .
+ /_______.'/  |__|.'.'| |// '. `._____.-'/
+ \_______|/     .'.'.-'  /    `-.______ /
+                .'   \_.'              `
+ .-,--.            ,-,---.     .  .
+ ' |   \ . ,-. ,-.  '|___/ . . |- |- ,-. ,-.
+ , |   / | `-. |    ,|   \ | | |  |  | | | |
+ `-^--'  ' `-' `-' `-^---' `-^ `' `' `-' ' '
+  ,--.             .     .-,--.         .      .
+ | `-' ,-. ,-. ,-. |- ,-. '|__/ ,-. ,-. |- ,-. |
+ |   . |   |-' ,-| |  |-' ,|    | | |   |  ,-| |
+ `--'  '   `-' `-^ `' `-' `'    `-' '   `' `-^ `'
+~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
+ The portal button on the creation disc.
+
+ Authors: Axel Kittenberger
+ License: MIT(Expat), see accompanying 'License'-file
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+
+/*
+| Export
+*/
+var Disc;
+Disc = Disc || { };
+
+
+/*
+| Imports
+*/
+var config;
+var Curve;
+var Dash;
+var Design;
+var Euclid;
+var Jools;
+var Proc;
+var shell;
+var theme;
+var Tree;
+
+
+/*
+| Capsule
+*/
+( function( ) {
+'use strict';
+
+
+if( typeof( window ) === 'undefined' )
+	{ throw new Error( 'this code needs a browser!' ); }
+
+/*
+| Constructor
+*/
+var DiscButtonCreatePortal = Disc.DiscButtonCreatePortal =
+	function( disc )
+{
+	Disc.DiscButton.call(
+		this,
+		disc,
+		'portal'
+	);
+};
+
+Jools.subclass( DiscButtonCreatePortal, Disc.DiscButton );
+
+
+/*
+| Draws the buttons icon
+*/
+DiscButtonCreatePortal.prototype.drawIcon =
+	function(
+		fabric
+	)
+{
+	var wx = 30;
+	var ny = 35;
+
+	fabric.fillText(
+		'Portal',
+		wx,
+		ny,
+		this.myStyle.font
+	);
+};
+
+
+} )( );
