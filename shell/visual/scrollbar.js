526fd2eae559798c01cb1020fe4f1c9bba793258
Axel Kittenberger <axkibe@gmail.com>
2013-07-23 08:10:14 +0200

    working

--- shell/visual/scrollbar.js
+++ shell/visual/scrollbar.js
@@ -36,56 +36,98 @@ if (typeof(window) === 'undefined')
 /*
 | Constructor.
 */
-var Scrollbar = Visual.Scrollbar = function()
+var Scrollbar =
+Visual.Scrollbar =
+	function( )
 {
-	this.visible    = false;
+	this.visible =
+		false;
 
 	// position
-	this._$pos      = 0;
+	this._$pos =
+		0;
 
 	// maximum position
-	this._$max      = null;
+	this._$max =
+		null;
 
 	// the size of the bar
-	this._$aperture = null;
+	this._$aperture =
+		null;
 
-	this._$zone     = null;
+	this._$zone =
+		null;
 };
 
 
 /*
 | Draws the scrollbar.
 */
-Scrollbar.prototype.draw = function(fabric, view)
+Scrollbar.prototype.draw =
+	function(
+		fabric,
+		view
+	)
 {
-	if (!this.visible)
-		{ throw new Error('Drawing an invisible scrollbar'); }
+	if( !this.visible )
+	{
+		throw new Error(
+			'Drawing an invisible scrollbar'
+		);
+	}
 
-	fabric.paint(theme.scrollbar.style, this.getArea(view), 'sketch', Euclid.View.proper);
+	fabric.paint(
+		theme.scrollbar.style,
+		this.getArea( view ),
+		'sketch',
+		Euclid.View.proper
+	);
 };
 
 
 /*
 | Returns the (2d) area of the scrollbar.
 */
-Scrollbar.prototype.getArea = function(view)
+Scrollbar.prototype.getArea =
+	function(
+		view
+	)
 {
-	var ths  = theme.scrollbar;
-	var pnw  = this._$pnw;
-	var size = this._$size;
-	var pos  = this._$pos;
-	var max  = this._$max;
+	var
+		ths =
+			theme.scrollbar,
 
-	var ap   = Math.round( this._$aperture * size / max );
-	var map  = Math.max( ap, ths.minSize );
-	var sy   = Math.round( pos * ( ( size - map + ap ) / max ) );
-	var s05  = Jools.half( ths.strength );
+		pnw =
+			this._$pnw,
 
-	return new Euclid.RoundRect(
-		view.point( pnw.x, pnw.y + sy       ).add( -s05, 0 ),
-		view.point( pnw.x, pnw.y + sy + map ).add(  s05, 0 ),
-		ths.ellipseA,
-		ths.ellipseB
+		size =
+			this._$size,
+
+		pos =
+			this._$pos,
+
+		max =
+			this._$max,
+
+		ap =
+			Math.round( this._$aperture * size / max ),
+
+		map =
+			Math.max( ap, ths.minSize ),
+
+		sy =
+			Math.round( pos * ( ( size - map + ap ) / max ) ),
+
+		s05 =
+			Jools.half( ths.strength );
+
+	return (
+		new Euclid.RoundRect(
+			view.point( pnw.x, pnw.y + sy ).add( -s05, 0 ),
+			view.point( pnw.x, pnw.y + sy + map ).add( s05, 0 ),
+			ths.ellipseA,
+			ths.ellipseB
+		)
 	);
 };
 
@@ -93,10 +135,13 @@ Scrollbar.prototype.getArea = function(view)
 /*
 | Returns the scrollbars position.
 */
-Scrollbar.prototype.getPos = function( )
+Scrollbar.prototype.getPos =
+	function( )
 {
 	if( !this.visible )
-		{ return 0; }
+	{
+		return 0;
+	}
 
 	return this._$pos;
 };
@@ -105,21 +150,47 @@ Scrollbar.prototype.getPos = function( )
 /*
 | Sets the scrollbars position and location.
 */
-Scrollbar.prototype.setPos = function( pos, aperture, max, pnw, size )
+Scrollbar.prototype.setPos =
+	function(
+		pos,
+		aperture,
+		max,
+		pnw,
+		size
+	)
 {
 	if( max - aperture >= 0 )
-		{ pos = Jools.limit( 0, pos, max - aperture ); }
+	{
+		pos =
+			Jools.limit( 0, pos, max - aperture );
+	}
 	else
-		{ pos = 0; }
+	{
+		pos =
+			0;
+	}
 
 	if( pos < 0 )
-		{ throw new Error( 'Scrollbar.setPos < 0' ); }
+	{
+		throw new Error(
+			'Scrollbar.setPos < 0'
+		);
+	}
 
-	this._$pos      = pos;
-	this._$aperture = aperture;
-	this._$max      = max;
-	this._$pnw      = pnw,
-	this._$size     = size;
+	this._$pos =
+		pos;
+
+	this._$aperture =
+		aperture;
+
+	this._$max =
+		max;
+
+	this._$pnw =
+		pnw;
+
+	this._$size =
+		size;
 };
 
 
@@ -129,11 +200,19 @@ Scrollbar.prototype.setPos = function( pos, aperture, max, pnw, size )
 Scrollbar.prototype.within = function( view, p )
 {
 	if( !this.visible )
-		{ return false; }
+	{
+		return false;
+	}
 
-	var pnw = this._$pnw;
-	var dex = view.dex(p);
-	var dey = view.dey(p);
+	var
+		pnw =
+			this._$pnw,
+
+		dex =
+			view.dex(p),
+
+		dey =
+			view.dey(p);
 
 	return (
 		dex >= pnw.x &&
@@ -147,7 +226,8 @@ Scrollbar.prototype.within = function( view, p )
 /*
 | Returns the value of pos change for d pixels in the current zone.
 */
-Scrollbar.prototype.scale = function(d)
+Scrollbar.prototype.scale =
+	function( d )
 {
 	return d * this._$max / this._$size;
 };
