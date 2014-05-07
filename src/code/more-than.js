bb8b98e0a52be2db3044a306fefee9f76ae75060
Axel Kittenberger <axkibe@gmail.com>
2014-05-07 09:26:10 +0200

    o

---
+++ src/code/more-than.js
@@ -0,0 +1,52 @@
+/*
+| Tests if a > b.
+|
+| Authors: Axel Kittenberger
+*/
+
+
+/*
+| Capsule
+*/
+(function() {
+'use strict';
+
+
+/*
+| The joobj definition.
+*/
+if( JOOBJ )
+{
+	return {
+		name :
+			'LessThan',
+		unit :
+			'Code',
+		attributes :
+			{
+				left :
+					{
+						comment :
+							'left expression',
+						type :
+							'Object'
+					},
+				right :
+					{
+						comment :
+							'right expression',
+						type :
+							'Object'
+					}
+			},
+		node :
+			true
+	};
+}
+
+
+module.exports =
+	require( '../joobj/this' )( module );
+
+
+} )( );
