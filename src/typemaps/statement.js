c821b3bdb9d32eb2854772824f1e88ca6e6ae09d
Axel Kittenberger <axkibe@gmail.com>
2014-06-02 07:59:19 +0200

    o

---
+++ src/typemaps/statement.js
@@ -0,0 +1,24 @@
+/*
+| Typemap code statements.
+*/
+module.exports =
+{
+	'And' :
+		'Code.And',
+	'Assign' :
+		'Code.Assign',
+	'Call' :
+		'Code.Call',
+	'Func' :
+		'Code.Func',
+	'New' :
+		'Code.New',
+	'ObjLiteral' :
+		'Code.ObjLiteral',
+	'Or' :
+		'Code.Or',
+	'Term' : // TODO
+		'Code.Term',
+	'Var' :
+		'Code.Var'
+};
