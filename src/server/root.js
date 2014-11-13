3f3dccb709c6541e2db2c1d6b2db358dc5ebe0e1
Axel Kittenberger <axkibe@gmail.com>
2014-11-13 09:09:29 +0100

    o

--- src/server/root.js
+++ src/server/root.js
@@ -162,6 +162,42 @@ server.root =
 
 prototype = server.root.prototype;
 
+/*
+| create replacement until root is a JION
+*/
+prototype.create =
+	function( )
+{
+	var
+		a,
+		arg,
+		aZ,
+		replace;
+
+	replace = new server.root( );
+
+	for( arg in this )
+	{
+		if( Object.isOwnProperty( this, arg ) )
+		{
+			replace[ arg ] = this[ arg ];
+		}
+	}
+
+	for(
+		a = 0, aZ = arguments.length;
+		a < aZ;
+		a++
+	)
+	{
+		replace[ arguments[ a ] ] = replace[ arguments[ a + 1 ] ];
+	}
+
+	root = replace;
+
+	return replace;
+};
+
 
 /*
 | Sets up the server.
