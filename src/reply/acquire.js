4bfd2a973a08f2ae64a17a3adc5bac72f3b9809b
Axel Kittenberger <axkibe@gmail.com>
2014-11-21 09:03:14 +0100

    o

---
+++ src/reply/acquire.js
@@ -0,0 +1,86 @@
+/*
+| The servers replies to a clients (space-)acquire request.
+*/
+
+
+/*
+| Exports
+*/
+var
+	request;
+
+request = request || { };
+
+
+/*
+| Capsule
+*/
+( function( ) {
+'use strict';
+
+
+/*
+| The jion definition.
+*/
+if( JION )
+{
+	return {
+		id :
+			'reply.acquire',
+		attributes :
+			{
+				status :
+					{
+						comment :
+							'the acquire result',
+						json :
+							true,
+						type :
+							'String'
+					},
+				access :
+					{
+						comment :
+							'access level, readonly(r) or read-write(rw)',
+						json :
+							true,
+						type :
+							'String',
+						defaultValue :
+							undefined
+					},
+				seq :
+					{
+						comment :
+							'sequence the space is at',
+						json :
+							true,
+						type :
+							'Integer',
+						defaultValue :
+							undefined
+					},
+				space :
+					{
+						comment :
+							'the space',
+						json :
+							true,
+						type :
+							'visual.space'
+					}
+
+			},
+		node :
+			true
+	};
+}
+
+
+if( SERVER )
+{
+	require( '../jion/this' )( module );
+}
+
+
+} )( );
