e25a8c47042f0e297b29148c7ab2b7b4bc09069d
Axel Kittenberger <axkibe@gmail.com>
2014-12-01 14:52:08 +0100

    o

--- src/ast/ast-array-literal.js
+++ src/ast/ast-array-literal.js
@@ -3,6 +3,10 @@
 */
 
 
+var
+	astArrayLiteral;
+
+
 /*
 | Capsule
 */
@@ -27,7 +31,73 @@ if( JION )
 }
 
 
-require( '../jion/this' )( module );
+astArrayLiteral = require( '../jion/this' )( module );
 
+
+/**/if( CHECK )
+/**/{
+/**/	var
+/**/		arg,
+/**/		r, rZ,
+/**/		util;
+/**/
+/**/	util = require( 'util' );
+/**/
+/***	/
+****	| Custom inspect
+****	/
+***/	astArrayLiteral.prototype.inspect =
+/**/		function(
+/**/			depth,
+/**/			opts
+/**/		)
+/**/	{
+/**/		var
+/**/			postfix,
+/**/			result;
+/**/
+/**/		if( !opts.ast )
+/**/		{
+/**/			result = 'ast{ ';
+/**/
+/**/			postfix = ' }';
+/**/		}
+/**/		else
+/**/		{
+/**/			result = postfix = '';
+/**/		}
+/**/
+/**/		opts.ast = true;
+/**/
+/**/		if( this.length === 0 )
+/**/		{
+/**/			result += '[ ]';
+/**/		}
+/**/		else
+/**/		{
+/**/			result += '[ ';
+/**/
+/**/			for(
+/**/				r = 0, rZ = this.length;
+/**/				r < rZ;
+/**/				r++
+/**/			)
+/**/			{
+/**/				arg = this.get( r );
+/**/
+/**/				if( r > 0 )
+/**/				{
+/**/					result += ', ';
+/**/				}
+/**/
+/**/				result += util.inspect( arg, opts );
+/**/			}
+/**/
+/**/			result += ' ]';
+/**/		}
+/**/
+/**/		return result + postfix;
+/**/	};
+/**/}
 
 } )( );
