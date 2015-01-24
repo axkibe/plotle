87cebbd45b3fa5891b8e5a557e1cfc2584b8f2eb
Axel Kittenberger <axkibe@gmail.com>
2015-01-24 18:29:12 +0100

    o

--- src/ast/greaterThan.js
+++ src/ast/greaterThan.js
@@ -25,14 +25,14 @@ if( JION )
 						comment :
 							'left expression',
 						type :
-							'Object'
+							'->astExpression'
 					},
 				right :
 					{
 						comment :
 							'right expression',
 						type :
-							'Object'
+							'->astExpression'
 					}
 			}
 	};
