2c754aa59c1da508676c31f2bb45648338397659
Axel Kittenberger <axkibe@gmail.com>
2015-02-04 11:09:51 +0100

    o

---
+++ src/jion/stringRay.js
@@ -0,0 +1,30 @@
+/*
+| A ray of strings.
+*/
+
+
+/*
+| Capsule.
+*/
+(function( ) {
+'use strict';
+
+
+/*
+| The jion definition.
+*/
+if( JION )
+{
+	return {
+		id :
+			'jion_stringRay',
+		ray :
+			[ 'String' ]
+	};
+}
+
+
+require( '../jion/this' )( module );
+
+
+} )( );
