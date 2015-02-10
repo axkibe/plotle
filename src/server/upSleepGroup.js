127be760480c686576cf1b11c2009e341ee2b8c2
Axel Kittenberger <axkibe@gmail.com>
2015-02-10 14:47:45 +0100

    o

---
+++ src/server/upSleepGroup.js
@@ -0,0 +1,30 @@
+/*
+| A client's update set to sleep.
+*/
+
+
+/*
+| Capsule.
+*/
+( function( ) {
+'use strict';
+
+
+/*
+| The jion definition.
+*/
+if( JION )
+{
+	return {
+		id :
+			'server_upSleepGroup',
+		group :
+			[ 'server_upSleep' ]
+	};
+}
+
+
+require( '../jion/this' )( module );
+
+
+} )( );
