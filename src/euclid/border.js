3e9a52248f7931f8f85bb9b98db0ab3d7502f43c
Axel Kittenberger <axkibe@gmail.com>
2015-03-02 14:42:07 +0100

    o

---
+++ src/euclid/border.js
@@ -0,0 +1,79 @@
+/*
+| A border.
+*/
+
+
+var
+	euclid_color,
+	euclid_border;
+
+
+/*
+| Capsule
+*/
+( function( ) {
+'use strict';
+
+
+/*
+| The jion definition.
+*/
+if( JION )
+{
+	return {
+		id :
+			'euclid_border',
+		attributes :
+			{
+				distance :
+					{
+						comment :
+							'distance from silhoutte',
+						type :
+							'number',
+						defaultValue :
+							'0'
+					},
+				width :
+					{
+						comment :
+							'border width',
+						type :
+							'number',
+						defaultValue :
+							'1'
+					},
+				color :
+					{
+						comment :
+							'color',
+						type :
+							'euclid_color'
+						// FIXME default to black
+					}
+			}
+	};
+}
+
+
+var
+	prototype;
+
+prototype = euclid_border.prototype;
+
+
+/*::::::::::::::::
+| default values
+::::::::::::::::*/
+
+
+/*
+| A simple black border
+*/
+euclid_border.simpleBlack =
+	euclid_border.create(
+		'color', euclid_color.black
+	);
+
+
+} )( );
---
+++ src/euclid/borderRay.js
@@ -0,0 +1,25 @@
+/*
+| A border ray.
+*/
+
+
+/*
+| Capsule
+*/
+( function( ) {
+'use strict';
+
+
+/*
+| The jion definition.
+*/
+if( JION )
+{
+	return {
+		id : 'euclid_borderRay',
+		ray : [ 'euclid_border' ]
+	};
+}
+
+
+} )( );
