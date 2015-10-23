d6f06eab0aea6a0a5d552218ae8b4762b91881e7
Axel Kittenberger <axkibe@gmail.com>
2015-10-23 08:05:56 +0200

    o

--- src/typemaps/visualItems.js
+++ src/typemaps/visualItems.js
@@ -1,13 +1,10 @@
 /*
 | Typemap of item visualisations.
-|
-| FIXME remove fabrics
 */
 module.exports =
 [
 	'visual_label',
 	'visual_note',
 	'visual_portal',
-	'fabric_para',
 	'visual_relation',
 ];
