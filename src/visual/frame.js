1510167c3d54f49cfecd9e553e0769c280a6b4b5
Axel Kittenberger <axkibe@gmail.com>
2015-12-10 09:26:31 +0100

    beginning new frame

---
+++ src/visual/frame.js
@@ -0,0 +1,45 @@
+/*
+| The alteration frame.
+*/
+
+
+/*
+| The jion definition.
+*/
+if( JION )
+{
+	throw{
+		id : 'visual_frame',
+		attributes :
+		{
+		}
+	};
+}
+
+
+var
+	visual_frame;
+
+/*
+| Capsule
+*/
+( function( ) {
+'use strict';
+
+
+if( NODE )
+{
+	require( 'jion' ).this( module, 'source' );
+
+	return;
+}
+
+
+var
+	prototype;
+
+prototype = visual_frame.prototype;
+
+
+
+} )( );
