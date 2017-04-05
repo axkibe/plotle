640b1e4eda064e5fd441403b7605a9e69bc36827
Axel Kittenberger <axkibe@gmail.com>
2017-04-05 18:00:07 +0200

    currently having webFontLoad and opentype

--- src/shell/fontloader.js
+++ src/shell/fontloader.js
@@ -6,8 +6,10 @@
 
 
 var
-	WebFont,
-	startup;
+	font_default,
+	opentype,
+	startup,
+	WebFont;
 
 
 /*
@@ -16,8 +18,44 @@ var
 ( function( ) {
 'use strict';
 
+var openTypeLoad;
+var	webFontLoad;
 
-window.onload =
+
+/*
+| Loads open type fonts.
+*/
+openTypeLoad =
+	function( )
+{
+	opentype.load(
+		//'fonts-DejaVuSans.ttf',
+		'fonts-OpenSans-Regular.ttf',
+		//'fonts-SourceSansPro-Regular.ttf',
+		//'fonts-Roboto-Regular.ttf',
+		//'fonts-Arimo-Regular.ttf',
+		function( err, font )
+	{
+		if (err)
+		{
+			console.log( 'Font could not be loaded: ' + err );
+		}
+		else
+		{
+			font_default = font;
+
+			startup( );
+		}
+	}
+	);
+};
+
+
+
+/*
+| Loads web fonts.
+*/
+webFontLoad =
 	function( )
 {
 	WebFont.load(
@@ -26,16 +64,19 @@ window.onload =
 		{
 			families:
 			[
-				'DejaVuSans',
-				'DejaVuSansBold'
+				'DejaVuSans'
 			]
 			//urls: [ '/fonts/dejavu.css' ], already in CSS
 		},
 
-		active: function() { startup( ); }
+		active: function() { openTypeLoad( ); }
 	}
 	);
 };
+
+
+//window.onload = openTypeLoad;
+window.onload = webFontLoad;
 
 
 } )( );
