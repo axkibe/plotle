3e3f843479e72eb6229c15d243f987b12c31994e
Axel Kittenberger <axkibe@gmail.com>
2017-04-30 10:37:13 +0200

    fix convert

--- src/tools/conv-12-to-13.js
+++ src/tools/conv-12-to-13.js
@@ -70,7 +70,17 @@ sus = require( 'suspend' );
 
 resume = sus.resume;
 
-json_typemap = require( '../json/typemap' );
+json_typemap = {
+	'euclid_point' : 'point',
+	'euclid_rect' : 'rect',
+	'gleam_point' : 'point',
+	'gleam_rect' : 'rect',
+
+	'fabric_label' : 'label',
+	'fabric_note' : 'note',
+	'fabric_portal' : 'portal',
+	'fabric_relation' : 'relation'
+}
 
 root = { };
 
