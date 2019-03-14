d4b2a5082d86fb674df5e8d2909c56be472ef679
Axel Kittenberger <axkibe@gmail.com>
2019-03-14 08:57:33 +0100

    combining fabric and visuals

--- src/fabric/label.js
+++ src/fabric/label.js
@@ -11,17 +11,42 @@ if( TIM )
 {
 	def.attributes =
 	{
+		// access level of current user (rw or ro)
+		// no json thus not saved or transmitted
+		access : { type : [ 'undefined', 'string' ] },
+
+		// current action
+		// no json thus not saved or transmitted
+		action : { type : [ 'undefined', '< ../action/types' ] },
+
 		// the labels document
 		doc : { type : '../fabric/doc', json : true },
 
 		// the fontsize of the label
 		fontsize : { type : 'number', json : true },
 
+		// the item is highlighted
+		// no json thus not saved or transmitted
+		highlight : { type : [ 'undefined', 'boolean' ] },
+
+		// node currently hovered upon
+		// no json thus not saved or transmitted
+		hover : { type : 'undefined' },
+
+		// the users mark
+		// no json thus not saved or transmitted
+		mark : { type : [ '< ../visual/mark/types', 'undefined' ] },
+
 		// the path of the doc
 		// no json thus not saved or transmitted
 		path : { type : [ 'undefined', 'tim.js/path' ] },
 
+		// position
 		pos : { type : '../gleam/point', json : true },
+
+		// the current space transform
+		// no json thus not saved or transmitted
+		transform : { type : [ 'undefined', '../gleam/transform' ] }
 	};
 
 	def.json = 'label';
--- src/fabric/relation.js
+++ src/fabric/relation.js
@@ -14,11 +14,27 @@ if( TIM )
 {
 	def.attributes =
 	{
+		// access level of current user (rw or ro)
+		// no json thus not saved or transmitted
+		access : { type : [ 'undefined', 'string' ] },
+
+		// current action
+		// no json thus not saved or transmitted
+		action : { type : [ 'undefined', '< ../action/types' ] },
+
 		// the labels document
 		doc : { type : './doc', json : true },
 
 		// the fontsize of the label
 		fontsize : { type : 'number', json : true },
+
+		// the item is highlighted
+		// no json thus not saved or transmitted
+		highlight : { type : [ 'undefined', 'boolean' ] },
+
+		// node currently hovered upon
+		// no json thus not saved or transmitted
+		hover : { type : 'undefined' },
 
 		// item the relation goes from
 		item1key : { type : 'string', json : true },
@@ -26,12 +42,19 @@ if( TIM )
 		// item the relation goes to
 		item2key : { type : 'string', json : true },
 
+		// the users mark
+		mark : { type : [ '< ../visual/mark/types', 'undefined' ] },
+
 		// the path of the doc
 		// no json thus not saved or transmitted
 		path : { type : [ 'undefined', 'tim.js/path' ] },
 
 		// position
 		pos : { type : '../gleam/point', json : true },
+
+		// the current space transform
+		// no json thus not saved or transmitted
+		transform : { type : [ 'undefined', '../gleam/transform' ] },
 	};
 
 	def.json = 'relation';
